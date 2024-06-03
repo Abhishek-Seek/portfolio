68817273b3d488c753e943574e01656136821211
softpro24 <darkworld1914@gmail.com>
2024-06-03 13:58:51 +0530

    webpage

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
+import Homepage from "@/components/Home";
+import Mystory from "@/components/Mystory";
+import Navbar from "@/components/Navbar";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
+
+ function Home() {
+  return (
+    
+    <div className="px-[5%] sm:px-[5%]">
+
+    <Navbar/>
+    <Homepage/>
+    <Projects/>
+    <Experience/>
+    <Skills/>
+    <Mystory/>
+    <Footer/>
+    </div>
+
+  );
+}
+
+export default Home
